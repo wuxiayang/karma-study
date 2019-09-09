220be16e3bac8b6c12f4b326a3ab86f9c8444683
admin <m18820136354_1@163.com>
2019-09-09 11:31:10 +0800

    安装环境配置

---
+++ notes/how-to-install.js
@@ -0,0 +1,40 @@
+初始化
+// npm init
+在karma-study  
+// npm i karma -g
+// npm i karma --save-dev
+// npm i mocha -g
+// npm i mocha  --save-dev
+// npm  i should --save-dev
+
+// karma init
+// Which testing framework do you want to use ?
+// Press tab to list possible options. Enter to move to the next question.
+// > mocha
+// Do you want to use Require.js ?
+// This will add Require.js plugin.
+// Press tab to list possible options. Enter to move to the next question.
+// > no
+// Do you want to capture any browsers automatically ?
+// Press tab to list possible options. Enter empty string to move to the next question.
+// > Chrome
+// > 
+// What is the location of your source and test files ?
+// You can use glob patterns, eg. "js/*.js" or "test/**/*Spec.js".
+// Enter empty string to move to the next question.
+// > 
+// Should any of the files included by the previous patterns be excluded ?
+// You can use glob patterns, eg. "**/*.swp".
+// Enter empty string to move to the next question.
+// > 
+// Do you want Karma to watch all the files and run the tests on change ?
+// Press tab to list possible options.
+// > yes
+// Config file generated at "/Users/admin/Desktop/1907/gotogithub/hub/karma-study/karma.conf.js".
+
+
+// npm i karma-qunit --save-dev
+// npm i karma-ie-launcher --save-dev
+
+启动浏览器测试
+// karma start  karma.conf.js
